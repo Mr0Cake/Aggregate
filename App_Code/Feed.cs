524afd4b12eeedf654ed512a6bf551f02e282e93
Mr0Cake <happy_kefke@hotmail.com>
2015-11-30 06:57:21 +0100

    Custom Date formatter

---
+++ App_Code/Feed.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Feed
+/// </summary>
+public class Feed
+{
+	public Feed()
+    {
+
+    }
+	private string _Title;
+
+	public string Title
+	{
+		get { return _Title;}
+		set { _Title = value;}
+	}
+
+    private string _Description;
+
+    public string Description
+    {
+        get { return _Description; }
+        set { _Description = value; }
+    }
+
+    private string _Content;
+
+    public string Content
+    {
+        get { return _Content; }
+        set { _Content = value; }
+    }
+
+    private DateTime _Date;
+
+    public DateTime Date
+    {
+        get { return _Date; }
+        set { _Date = value; }
+    }
+
+    private string _Creator;
+
+    public string Creator
+    {
+        get { return _Creator; }
+        set { _Creator = value; }
+    }
+
+    private string[] _Category;
+
+    public string[] Category
+    {
+        get { return _Category; }
+        set { _Category = value; }
+    }
+
+    private string _Link;
+
+    public string Link
+    {
+        get { return _Link; }
+        set { _Link = value; }
+    }
+
+	
+	
+}
---
+++ App_Code/FeedParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Xml;
+
+/// <summary>
+/// Summary description for FeedParser
+/// </summary>
+public class FeedParser
+{
+
+    private List<SyndicationFeed> _Feeds;
+
+    public List<SyndicationFeed> Feeds
+    {
+        get { return _Feeds; }
+        set { _Feeds = value; }
+    }
+    
+	public FeedParser()
+	{
+		
+	}
+    public void addFeeds(List<string> items)
+    {
+        foreach (string item in items)
+        {
+            Uri feedUri = new Uri(item); 
+            SyndicationFeed syndicationFeed;
+            using (XmlReader reader = XmlReader.Create(feedUri.AbsoluteUri))
+            {
+                syndicationFeed = SyndicationFeed.Load(reader);
+            }
+            syndicationFeed.Id = item;
+
+            Feeds.Add(syndicationFeed);
+        }
+    }
+}
